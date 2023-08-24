209a13ad99fa9e9a2ef2e6d3d505f517a9a4ca9e
Arina231 <arinahonaa119@gmail.com>
2023-08-24 18:05:17 +0100

    dice rolling generated

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 'use strict';
 //selecting elements///
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. roll to get random number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //2. display dice
+  diceEl.classList.remove('hidden');
+
+  diceEl.src = `dice-${dice}.png`;
+  //3. check for number 1
+  if (dice != 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  }
+});
